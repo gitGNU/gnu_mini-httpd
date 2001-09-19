9802d4f58f839375223f7254d4cc4b796893ec55
Peter Simons <simons@cryp.tyo>
2001-09-19 17:39:31 +0000

    Rewrote process_input() to use regexec() directly in order to avoid excessive use of std::string.

--- rh-process-input.cc
+++ rh-process-input.cc
@@ -12,46 +12,47 @@ const RegExp RequestHandler::get_regex("^GET +([^ ]+) +HTTP/([0-9]+)\\.([0-9]+)"
 const RegExp RequestHandler::host_port_regex("^Host: +([^ ]+):[0-9]+", REG_EXTENDED);
 const RegExp RequestHandler::host_regex("^Host: +([^ ]+)", REG_EXTENDED);
 
-bool RequestHandler::process_input(const char* begin, const char* end)
+bool RequestHandler::process_input(char* begin, char* end)
     {
     TRACE();
-    string line(begin, end-begin);
-    if (!line.empty())
+
+    if (begin == end)
+	return true;
+
+    char backup = *end;
+    *end = '\0';
+
+    static regmatch_t pmatch[10];
+    if (host.empty() && url.empty() && regexec(full_get_port_regex, begin, sizeof(pmatch) / sizeof(regmatch_t), pmatch, 0) == 0)
 	{
-	vector<string> vec;
-	if (host.empty() && url.empty() && full_get_port_regex.submatch(line, vec))
-	    {
-	    host = vec[1];
-	    url  = vec[2];
-	    debug("%d: Got full GET request including port: host = '%s' and url = '%s'.", sockfd, host.c_str(), url.c_str());
-	    }
-	else if (host.empty() && url.empty() && full_get_regex.submatch(line, vec))
-	    {
-	    host = vec[1];
-	    url  = vec[2];
-	    debug("%d: Got full GET request: host = '%s' and url = '%s'.", sockfd, host.c_str(), url.c_str());
-	    }
-	else if (url.empty() && get_regex.submatch(line, vec))
-	    {
-	    url = vec[1];
-	    debug("%d: Got GET request: url = '%s'.", sockfd, url.c_str());
-	    }
-	else if (host.empty() && host_port_regex.submatch(line, vec))
-	    {
-	    host = vec[1];
-	    debug("%d: Got Host header including port: host = '%s'.", sockfd, host.c_str());
-	    }
-	else if (host.empty() && host_regex.submatch(line, vec))
-	    {
-	    host = vec[1];
-	    debug("%d: Got Host header: host = '%s'.", sockfd, host.c_str());
-	    }
-	else
-	    {
-	    debug("%d: Got unknown header '%s'.", sockfd, line.c_str());
-	    }
-	return false;
+	host.assign(begin + pmatch[1].rm_so, pmatch[1].rm_eo - pmatch[1].rm_so);
+	url.assign(begin + pmatch[2].rm_so, pmatch[2].rm_eo - pmatch[2].rm_so);
+	debug("%d: Got full GET request including port: host = '%s' and url = '%s'.", sockfd, host.c_str(), url.c_str());
+	}
+    else if (host.empty() && url.empty() && regexec(full_get_regex, begin, sizeof(pmatch) / sizeof(regmatch_t), pmatch, 0) == 0)
+	{
+	host.assign(begin + pmatch[1].rm_so, pmatch[1].rm_eo - pmatch[1].rm_so);
+	url.assign(begin + pmatch[2].rm_so, pmatch[2].rm_eo - pmatch[2].rm_so);
+	debug("%d: Got full GET request: host = '%s' and url = '%s'.", sockfd, host.c_str(), url.c_str());
+	}
+    else if (url.empty() && regexec(get_regex, begin, sizeof(pmatch) / sizeof(regmatch_t), pmatch, 0) == 0)
+	{
+	url.assign(begin + pmatch[1].rm_so, pmatch[1].rm_eo - pmatch[1].rm_so);
+	debug("%d: Got GET request: url = '%s'.", sockfd, url.c_str());
+	}
+    else if (host.empty() && regexec(host_port_regex, begin, sizeof(pmatch) / sizeof(regmatch_t), pmatch, 0) == 0)
+	{
+	host.assign(begin + pmatch[1].rm_so, pmatch[1].rm_eo - pmatch[1].rm_so);
+	debug("%d: Got host header including port: host = '%s'.", sockfd, host.c_str());
+	}
+    else if (host.empty() && regexec(host_regex, begin, sizeof(pmatch) / sizeof(regmatch_t), pmatch, 0) == 0)
+	{
+	host.assign(begin + pmatch[1].rm_so, pmatch[1].rm_eo - pmatch[1].rm_so);
+	debug("%d: Got host header: host = '%s'.", sockfd, host.c_str());
 	}
     else
-	return true;
+	debug("%d: Got unknown header '%s'.", sockfd, begin);
+
+    *end = backup;
+    return false;
     }
