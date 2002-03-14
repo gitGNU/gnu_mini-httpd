217480e5a2c1be356a93ed1041114ead320f96fb
Peter Simons <simons@cryp.tyo>
2002-03-14 17:06:17 +0000

    Use ScopeGuard to make sure resources are freed in case of exceptions.

--- tcp-listener.hh
+++ tcp-listener.hh
@@ -13,6 +13,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include "ScopeGuard/ScopeGuard.hh"
 #include "system-error/system-error.hh"
 #include "libscheduler/scheduler.hh"
 #include "log.hh"
@@ -27,37 +28,33 @@ class TCPListener : public scheduler::event_handler
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd == -1)
 	    throw system_error("socket() failed");
+        ScopeGuard sg_sockfd = MakeGuard(close, sockfd);
 
-	try {
-	    int true_flag = 1;
-	    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &true_flag, sizeof(int)) == -1)
-		throw system_error("Can't set listen socket to REUSEADDR mode");
+        int true_flag = 1;
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &true_flag, sizeof(int)) == -1)
+            throw system_error("Can't set listen socket to REUSEADDR mode");
 
-	    if (fcntl(sockfd, F_SETFL, O_NONBLOCK) == -1)
-		throw system_error("Can't set listen socket to non-blocking mode");
+        if (fcntl(sockfd, F_SETFL, O_NONBLOCK) == -1)
+            throw system_error("Can't set listen socket to non-blocking mode");
 
-	    sin.sin_family      = AF_INET;
-	    sin.sin_addr.s_addr = htonl(INADDR_ANY);
-	    sin.sin_port        = htons(port_no);
-	    sin_size            = sizeof(sin);
-	    if (bind(sockfd, (sockaddr*)&sin, sin_size) == -1)
-		throw system_error("bind() failed");
+        sin.sin_family      = AF_INET;
+        sin.sin_addr.s_addr = htonl(INADDR_ANY);
+        sin.sin_port        = htons(port_no);
+        sin_size            = sizeof(sin);
+        if (bind(sockfd, (sockaddr*)&sin, sin_size) == -1)
+            throw system_error("bind() failed");
 
-	    if (listen(sockfd, queue_backlog) == -1)
-		throw system_error("listen() failed");
+        if (listen(sockfd, queue_backlog) == -1)
+            throw system_error("listen() failed");
 
-	    scheduler::handler_properties prop;
-	    prop.poll_events  = POLLIN;
-	    prop.read_timeout = 0;
-	    mysched.register_handler(sockfd, *this, prop);
-	    }
-	catch(...)
-	    {
-	    close(sockfd);
-	    throw;
-	    }
+        scheduler::handler_properties prop;
+        prop.poll_events  = POLLIN;
+        prop.read_timeout = 0;
+        mysched.register_handler(sockfd, *this, prop);
 
-	info("Listening on TCP port %d for incoming requests ...", port_no);
+        info("Listening on TCP port %d for incoming requests ...", port_no);
+
+        sg_sockfd.Dismiss();
 	}
 
     virtual ~TCPListener()
@@ -77,16 +74,19 @@ class TCPListener : public scheduler::event_handler
 	    error("TCPListener: Failed to accept new connection with accept(): %s.", strerror(errno));
 	    return;
 	    }
-	try { new connection_handlerT(mysched, streamfd, sin); }
+        ScopeGuard sg_streamfd = MakeGuard(close, streamfd);
+	try
+            {
+            new connection_handlerT(mysched, streamfd, sin);
+            sg_streamfd.Dismiss();
+            }
 	catch(const std::exception& e)
 	    {
 	    error("TCPListener: Caught exception while creating connection handler: %s", e.what());
-	    close(streamfd);
 	    }
 	catch(...)
 	    {
 	    error("TCPListener: Caught unknown exception while creating connection handler.");
-	    close(streamfd);
 	    }
 	}
     virtual void fd_is_writable(int)
