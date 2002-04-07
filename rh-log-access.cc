3d4350f7267f5fff1e49ed376f6300012327f2e1
Peter Simons <simons@cryp.tyo>
2002-04-07 02:54:40 +0000

    Fixed log_access() for the new data structures.

--- rh-log-access.cc
+++ rh-log-access.cc
@@ -8,51 +8,74 @@
 #include "system-error/system-error.hh"
 #include "RequestHandler.hh"
 #include "timestamp-to-string.hh"
+#include "search-and-replace.hh"
 #include "config.hh"
 #include "log.hh"
 
 using namespace std;
 
-#if 0
-void RequestHandler::log_access() const throw()
+static inline string escape_quotes(const string& input)
     {
+    return search_and_replace(input, "\"", "\\\"", true);
+    }
+
+void RequestHandler::log_access()
+    {
+    TRACE();
+
+    // Shouldn't happen ... But better check rather than to log
+    // nonsense.
+
+    if (request.status_code.empty())
+        {
+        error("Can't write access log entry for connection with %d because there is no status code.", peer_address);
+        return;
+        }
+
+    // Construct the path of the logfile.
+
+    string logfile = config->logfile_directory;
+    if (request.host.empty())
+        logfile += "no-hostname";
+    else
+        logfile += request.host + "-access";
+
+    // Convert the object size now, so that we can write a string.
+    // That's necessary, because in some cases we write "-" rather
+    // than a number.
+
+    char object_size[32];
+    if (request.object_size.empty())
+        strcpy("-", object_size);
+    else
+        {
+        int len = snprintf(object_size, sizeof(object_size), "%u", request.object_size.data());
+        if (len < 0 || len > static_cast<int>(sizeof(object_size)))
+            {
+            error("Internal error: While formatting object_size, snprintf() exceeded the internal buffer.");
+            // Just kidding.
+            }
+        }
+    // Open it and write the entry.
+
+    FILE* fh = fopen(logfile.c_str(), "a");
+    if (fh == 0)
+        throw system_error(string("Can't open logfile '") + logfile + "'");
+
     try
         {
-        TRACE();
-
-        // Construct the path of the logfile.
-
-        char filename[4096];
-        int len = snprintf(filename, sizeof(filename), config->logfile, host.c_str());
-        if (len < 0 || len >= static_cast<int>(sizeof(filename)))
-            throw length_error("logfile path exceeds size of internal buffer");
-        debug(("logfile = '%s'", filename));
-
-        // Create the data for the log entry.
-
-        string timestamp = time_to_logdate(time(0));
-
-        // Open it and write the entry.
-
-        FILE* fh = fopen(filename, "a");
-        if (fh == 0)
-            throw system_error("Can't open logfile");
-
-        fprintf(fh, "%s - - [%s] \"%s %s HTTP/%u.%u\" %u %u \"%s\" \"%s\"\n",
-                peer_addr_str, timestamp.c_str(), method.c_str(),
-                escape_quotes(path).c_str(), major_version, minor_version,
-                returned_status_code, returned_object_size,
-                escape_quotes(referer).c_str(),
-                escape_quotes(user_agent).c_str());
-
-        fclose(fh);
-        }
-    catch(const exception& e)
-        {
-        error("Caught exception: %s", e.what());
+        fprintf(fh, "%s - - [%s] \"%s %s HTTP/%u.%u\" %u %s \"%s\" \"%s\"\n",
+                peer_address, time_to_logdate(request.start_up_time).c_str(),
+                request.method.c_str(), escape_quotes(request.url.path).c_str(),
+                request.major_version, request.minor_version,
+                request.status_code.data(), object_size,
+                escape_quotes(request.referer).c_str(),
+                escape_quotes(request.user_agent).c_str());
         }
     catch(...)
         {
+        fclose(fh);
+        throw;
         }
+    fclose(fh);
     }
-#endif
