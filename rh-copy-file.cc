54a2b307318443a626a8c619410c130c70d9d243
Peter Simons <simons@cryp.tyo>
2002-04-07 03:12:22 +0000

    Added better error messages.

--- rh-copy-file.cc
+++ rh-copy-file.cc
@@ -9,6 +9,13 @@
 
 using namespace std;
 
+/*
+   In COPY_FILE state, all we do is fill up the write_buffer with data
+   when it's empty, and if the file is through, we'll go into any of
+   the FLUSH_BUFFER states -- depending on whether we support
+   persistent connections or not -- to go on.
+*/
+
 bool RequestHandler::copy_file()
     {
     TRACE();
@@ -18,7 +25,7 @@ bool RequestHandler::copy_file()
         char buf[4096];
         ssize_t rc = read(filefd, buf, sizeof(buf));
         if (rc < 0)
-            throw system_error("read() from hard disk failed");
+            throw system_error(string("read() from file '") + filename + "' failed");
         else if (rc == 0)
             {
             debug(("%d: The complete file is copied: going into FLUSH_BUFFER_AND_TERMINATE state.", sockfd));
