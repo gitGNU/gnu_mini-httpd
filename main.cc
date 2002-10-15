1105ed821edf220b9da148794b949ea6016244e6
Peter Simons <simons@cryp.tyo>
2002-10-15 18:56:05 +0000

    Cast signal handlers to avoid warnings.

--- main.cc
+++ main.cc
@@ -35,10 +35,10 @@ try
 
     // Install signal handler.
 
-    signal(SIGTERM, &set_sig_term);
-    signal(SIGINT, &set_sig_term);
-    signal(SIGHUP, &set_sig_term);
-    signal(SIGQUIT, &set_sig_term);
+    signal(SIGTERM, (sighandler_t)&set_sig_term);
+    signal(SIGINT, (sighandler_t)&set_sig_term);
+    signal(SIGHUP, (sighandler_t)&set_sig_term);
+    signal(SIGQUIT, (sighandler_t)&set_sig_term);
     signal(SIGPIPE, SIG_IGN);
 
     // Start-up scheduler and listener.
