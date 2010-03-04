b079d663b9085ff800af6dc03bb4b4f19b337689
Peter Simons <simons@cryp.to>
2010-03-04 23:14:57 +0100

    NEWS: documented recent user-visible changes in the 1.1 release

--- cfg.mk
+++ cfg.mk
@@ -2,7 +2,7 @@
 
 gnu_rel_host    := dl.sv.nongnu.org
 upload_dest_dir_:= /releases/mini-httpd/
-old_NEWS_hash   := d41d8cd98f00b204e9800998ecf8427e
+old_NEWS_hash   := af0e7009fb0519c60c0639bdf57e3aea
 gpg_key_ID      := 99089D72
 url_dir_list    := http://download.savannah.nongnu.org/releases/mini-httpd
 today           := $(date "+%Y-%m-%d")
