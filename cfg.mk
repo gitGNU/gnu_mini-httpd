f9477f608b6bb9956cee247e7daa28f0de4e9309
Peter Simons <simons@cryp.to>
2013-02-09 22:48:16 +0100

    maint: post-release administrivia
    * NEWS: Add header line for next release.
    * .prev-version: Record previous version.
    * cfg.mk (old_NEWS_hash): Auto-update.

--- cfg.mk
+++ cfg.mk
@@ -2,7 +2,7 @@
 
 gnu_rel_host    := dl.sv.nongnu.org
 upload_dest_dir_:= /releases/mini-httpd/
-old_NEWS_hash   := b9c5aa6710ad161493097a91d2f62a5f
+old_NEWS_hash   := f4a21f4b1fe7d5427f247ad0e4e3016b
 gpg_key_ID      := 99089D72
 url_dir_list    := http://download.savannah.nongnu.org/releases/mini-httpd
 today           := $(date "+%Y-%m-%d")
