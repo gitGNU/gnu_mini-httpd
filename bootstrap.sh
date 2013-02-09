f18faae584aaa5004602ead0b5cd5548cfb5f533
Peter Simons <simons@cryp.to>
2013-02-09 22:44:56 +0100

    bootstrap.sh: adapt the set of warnings for the upcoming release

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,6 +13,11 @@ gnulib_modules=( git-version-gen gitlog-to-changelog gnupload
 
 $gnulibtool --m4-base build-aux --source-base libgnu --import "${gnulib_modules[@]}"
 
+sed -i -e 's/^sc_error_message_uppercase/disabled_sc_error_message_uppercase/' \
+       -e 's/^sc_prohibit_path_max_allocation/disabled_sc_prohibit_path_max_allocation/' \
+       -e 's/^sc_unmarked_diagnostics/disabled_sc_unmarked_diagnostics/' \
+    maint.mk
+
 build-aux/gitlog-to-changelog >>ChangeLog
 
 autoreconf --install -Wall
