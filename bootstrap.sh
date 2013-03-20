7514c7b6633e824e3a34b96851c877595063b163
Peter Simons <simons@cryp.to>
2013-03-20 14:23:04 +0100

    bootstrap.sh: avoid use of 'sed -i', which doesn't exist on BSD

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,10 +13,10 @@ gnulib_modules=( git-version-gen gitlog-to-changelog gnupload
 
 $gnulibtool --m4-base build-aux --source-base libgnu --import "${gnulib_modules[@]}"
 
-sed -i -e 's/^sc_error_message_uppercase/disabled_sc_error_message_uppercase/' \
-       -e 's/^sc_prohibit_path_max_allocation/disabled_sc_prohibit_path_max_allocation/' \
-       -e 's/^sc_unmarked_diagnostics/disabled_sc_unmarked_diagnostics/' \
-    maint.mk
+sed -e 's/^sc_error_message_uppercase/disabled_sc_error_message_uppercase/' \
+    -e 's/^sc_prohibit_path_max_allocation/disabled_sc_prohibit_path_max_allocation/' \
+    -e 's/^sc_unmarked_diagnostics/disabled_sc_unmarked_diagnostics/' \
+    <maint.mk >maint.mk-new && mv maint.mk-new maint.mk
 
 build-aux/gitlog-to-changelog >>ChangeLog
 
